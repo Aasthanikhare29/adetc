8428347c5c2b12c7261e509227fded68dc821392
Aasthanikhare29 <nikhareaastha@gmail.com>
2026-08-18 10:23:33 +0530

    Load vendor CSS and web fonts in layout (match Vercel styling)

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,6 +19,14 @@ export default function RootLayout({ children }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="web icon" href="/assets/images/favicon.ico" />
+        <link rel="stylesheet" href="/assets/webfonts/font-family-anton.css" />
+        <link rel="stylesheet" href="/assets/webfonts/font-family-poppins.css" />
+        <link rel="stylesheet" href="/assets/css/vendor/bootstrap.min.css" />
+        <link rel="stylesheet" href="/assets/css/vendor/fontawesome.css" />
+        <link rel="stylesheet" href="/assets/css/vendor/solid.css" />
+        <link rel="stylesheet" href="/assets/css/vendor/regular.css" />
+        <link rel="stylesheet" href="/assets/css/vendor/brands.css" />
+        <link rel="stylesheet" href="/assets/css/vendor/swiper-bundle.min.css" />
         <link rel="stylesheet" href="/assets/css/main.css" />
         <link rel="stylesheet" href="/assets/css/responsive.css" />
       </head>
